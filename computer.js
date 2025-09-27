2b6bb734cd5a687035b22ffc89d3da614256dd93
Aurangzaibofficial <muhammadaurangzaib2005@gmail.com>
2025-09-27 22:55:18 +0500

    g

---
+++ computer.js
@@ -0,0 +1,38 @@
+async function getQueryAnswer(){
+        try {
+          const divRef = document.querySelector("#computer");
+             const queryInputRef = document.querySelector("#query");
+             const query = queryInputRef.value;
+             const res = await axios.post(
+                "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
+                {
+    "contents": [
+      {
+        "parts": [
+            {
+                text:"You are computer programmer and tell anything about computer , if other thing is asked said query is wrong",
+
+            },
+          {
+            text: query,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    headers: 
+  {  "X-goog-api-key":"AIzaSyApmncPeFD46sQx80XS9ioN7RGnXQrM9UM",
+    "Content-Type":" application/json",
+},
+
+  }
+             );
+             const answer = res.data.candidates[0].content.parts[0].text;
+             divRef.innerHTML = answer;
+            
+
+        } catch (err){
+            alert("Something went wrong");
+        }
+    }
